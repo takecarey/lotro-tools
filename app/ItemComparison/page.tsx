ee05128e414a56629f0e7a86f1459990bd02274b
takecarey <trevorcarey13@gmail.com>
2024-11-30 11:37:09 -0500

    Updates

--- app/ItemComparison/page.tsx
+++
@@ -1,9 +0,0 @@
-import ItemComparison from '@/components/ItemComparison';
-
-export default function ItemComparisonPage() {
-  return (
-    <main className="container mx-auto p-4">
-      <ItemComparison />
-    </main>
-  );
-}
--- app/StatCalculator/page.tsx
+++
@@ -1,9 +0,0 @@
-import StatCalculator from '@/components/StatCalculator';
-
-export default function StatCalculatorPage() {
-  return (
-    <main className="container mx-auto p-4">
-      <StatCalculator />
-    </main>
-  );
-}
